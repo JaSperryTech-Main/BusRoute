784276c44161f3c1ce580e2bbdc9b5d93154b8a8
JaSperryTech <jsperry0807+git@gmail.com>
2024-12-13 18:14:36 -0500

    Revert "Update for Public"
    This reverts commit 3d616ec7d3753a5fc40764a3cbca6b0e9b9ea030.

--- server/api/index.js
+++
@@ -1,4 +0,0 @@
-// api/index.js
-const app = require("../index"); // Import your Express app
-const serverless = require("serverless-http"); // Serverless HTTP for Express
-module.exports.handler = serverless(app); // Export handler for Vercel
